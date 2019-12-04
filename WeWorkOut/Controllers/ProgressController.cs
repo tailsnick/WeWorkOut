7c9be5324b62b393098da54a8c47f5b4ba9b50ae
Rachel Hoffman <tailsnick@yahoo.com>
2019-12-04 14:50:41 -0700

    Progress fixed for dates and switch case in controller.
    NOTES: The button on the Progress index does not work yet, and the Progress controller json is not sent back correctly.

--- WeWorkOut/Controllers/ProgressController.cs
+++ WeWorkOut/Controllers/ProgressController.cs
@@ -39,18 +39,59 @@ namespace WeWorkOut.Controllers
         }
 
         //Get data for that exercise, from the user, and sends it back to the javascript
-        public async Task<JsonResult> GetSubmitedData(int exerciseID)
+        public async Task<JsonResult> GetSubmittedData(int exerciseID, string goalType)
         {
+            // DB Query to get goal from DB
+
+            // Examine the goal, identify the exercise
+
+            // Examine the goal, identify the measurement type (time, reps, weight, etc.)
+
+            // Query the Exercise record DB.  Using:  Exercise = exercise, userid = userid, where measurement type (for that goal) != null
+
+            // LIst<DateTime> x = new LIst<DAteTime
+            // List<int> y = new Ist<int>
+
+            // for each thing in ExerciseREcords
+            // x.add(er.datetime)
+
             //////////////PLACE HOLDER CODE!!!!!!!//////////
-            
+            List<ExerciseRecord> records = new List<ExerciseRecord>();
+
             // Extract the user's ID.
             string userID = User.Claims.First().Value;
 
-            // Get the exercise records associated with that user
-            List<ExerciseRecord> records = await _context.ExerciseRecord
-                .FromSqlInterpolated($"SELECT * FROM ExerciseRecord WHERE ExcerciseID={exerciseID} AND UserID={userID}")
-                .Include(er => er.Exercise)
-                .ToListAsync();
+            // Get the exercise records associated with that user, that exerciseID and that specific column being not null
+            switch (goalType) 
+            {
+                case "Time":
+                    records = await _context.ExerciseRecord
+                        .FromSqlInterpolated($"SELECT * FROM ExerciseRecord WHERE ExcerciseID={exerciseID} AND UserID={userID} AND TimeQuantity IS NOT NULL")
+                        .Include(er => er.Exercise)
+                        .ToListAsync();
+                    break;
+                case "Weight":
+                    records = await _context.ExerciseRecord
+                        .FromSqlInterpolated($"SELECT * FROM ExerciseRecord WHERE ExcerciseID={exerciseID} AND UserID={userID} AND WeightQuantity IS NOT NULL")
+                        .Include(er => er.Exercise)
+                        .ToListAsync();
+                    break;
+                case "Reps":
+                    records = await _context.ExerciseRecord
+                        .FromSqlInterpolated($"SELECT * FROM ExerciseRecord WHERE ExcerciseID={exerciseID} AND UserID={userID} AND RepQuantity IS NOT NULL")
+                        .Include(er => er.Exercise)
+                        .ToListAsync();
+                    break;
+                case "Distance":
+                    records = await _context.ExerciseRecord
+                        .FromSqlInterpolated($"SELECT * FROM ExerciseRecord WHERE ExcerciseID={exerciseID} AND UserID={userID} AND DistanceQuantity IS NOT NULL")
+                        .Include(er => er.Exercise)
+                        .ToListAsync();
+                    break;
+            }
+
+            
+            
 
             if (records != null)
             {
